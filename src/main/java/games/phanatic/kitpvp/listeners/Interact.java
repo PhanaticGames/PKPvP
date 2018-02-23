0e70f9d4af45cee7779110fd0f79432b5c179130
Matthew <mattparksjr@gmail.com>
2018-02-22 20:45:32 -0500

    Added steak-eating system

--- src/main/java/games/phanatic/kitpvp/listeners/Interact.java
+++ src/main/java/games/phanatic/kitpvp/listeners/Interact.java
@@ -1,9 +1,13 @@
 package games.phanatic.kitpvp.listeners;
 
 import games.phanatic.kitpvp.PKPvP;
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class Interact implements Listener {
 
@@ -15,6 +19,19 @@ public class Interact implements Listener {
 
     @EventHandler
     public void execute(PlayerInteractEvent e) {
+        if (e.getAction() == Action.LEFT_CLICK_BLOCK || e.getAction() == Action.LEFT_CLICK_AIR) {
+            if (e.getPlayer().getItemInHand().getType() == Material.COOKED_BEEF) {
+                e.getPlayer().setHealth(e.getPlayer().getHealth() + 3F);
+                ItemStack stack = e.getItem();
+                if (stack.getAmount() >= 2) {
+                    ItemStack newStack = new ItemStack(Material.COOKED_BEEF, stack.getAmount() - 1);
+                    e.getPlayer().setItemInHand(newStack);
+                } else {
+                    e.getPlayer().setItemInHand(null);
+                }
+                e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.EAT, 1.0F, 1.0F);
+            }
+        }
         if (e.getItem().getType() == pvp.getIsManager().getKitSelector().getType()) {
             pvp.getInvManager().openKitSelector(e.getPlayer());
         } else if (e.getItem().getType() == pvp.getIsManager().getKillStreaks().getType()) {
